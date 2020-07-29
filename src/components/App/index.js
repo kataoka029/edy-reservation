84b180bdb20c8642d1f3e21c4a9e3557dd91c8aa
kataoka029 <kataoka029@gmail.com>
2020-07-29 22:45:33 +0900

    First commit

---
+++ src/components/App/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.scss";
+
+const App = () => {
+  return (
+    <div className="App">
+      <p>hello world</p>
+    </div>
+  );
+};
+
+export default App;
